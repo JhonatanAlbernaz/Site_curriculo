81dedf5045a168db1e6674c8e8f854a924b0f0a5
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-10-14 05:36:31 -0300

    Melhorando a estrutura do meu site portfólio

---
+++ js/app.js
@@ -0,0 +1,63 @@
+$("#imagem_principal").on("mouseover", function(e) {
+    $("#imagem_principal").css("max-width", "100%");
+    $("#imagem_principal").css("-moz-transition", "all 0.3s");
+    $("#imagem_principal").css("-webkit-transition", "all 0.3s");
+    $("#imagem_principal").css("transition", "all 0.3s");
+    $("#imagem_principal").css("-moz-transform", "scale(1.1)");
+    $("#imagem_principal").css("-webkit-transform", "scale(1.1)");
+  });
+  
+  $("#imagem_principal").on("mouseleave", function(e) {
+    $("#imagem_principal").css("-moz-transform", "");
+    $("#imagem_principal").css("-webkit-transform", "");
+  });
+
+  //====================================================================//
+
+  $("#icone_fa_whatsapp").on("mouseover", function(e) {
+    $("#icone_fa_whatsapp").css("max-width", "100%");
+    $("#icone_fa_whatsapp").css("-moz-transition", "all 0.3s");
+    $("#icone_fa_whatsapp").css("-webkit-transition", "all 0.3s");
+    $("#icone_fa_whatsapp").css("transition", "all 0.3s");
+    $("#icone_fa_whatsapp").css("-moz-transform", "scale(1.1)");
+    $("#icone_fa_whatsapp").css("-webkit-transform", "scale(1.1)");
+  });
+  
+  $("#icone_fa_whatsapp").on("mouseleave", function(e) {
+    $("#icone_fa_whatsapp").css("-moz-transform", "");
+    $("#icone_fa_whatsapp").css("-webkit-transform", "");
+  });
+
+  //====================================================================//
+
+  $("#icone_fa_instagram").on("mouseover", function(e) {
+    $("#icone_fa_instagram").css("max-width", "100%");
+    $("#icone_fa_instagram").css("-moz-transition", "all 0.3s");
+    $("#icone_fa_instagram").css("-webkit-transition", "all 0.3s");
+    $("#icone_fa_instagram").css("transition", "all 0.3s");
+    $("#icone_fa_instagram").css("-moz-transform", "scale(1.1)");
+    $("#icone_fa_instagram").css("-webkit-transform", "scale(1.1)");
+  });
+  
+  $("#icone_fa_instagram").on("mouseleave", function(e) {
+    $("#icone_fa_instagram").css("-moz-transform", "");
+    $("#icone_fa_instagram").css("-webkit-transform", "");
+  });
+
+  //====================================================================//
+
+  $("#icone_fa_github").on("mouseover", function(e) {
+    $("#icone_fa_github").css("max-width", "100%");
+    $("#icone_fa_github").css("-moz-transition", "all 0.3s");
+    $("#icone_fa_github").css("-webkit-transition", "all 0.3s");
+    $("#icone_fa_github").css("transition", "all 0.3s");
+    $("#icone_fa_github").css("-moz-transform", "scale(1.1)");
+    $("#icone_fa_github").css("-webkit-transform", "scale(1.1)");
+  });
+  
+  $("#icone_fa_github").on("mouseleave", function(e) {
+    $("#icone_fa_github").css("-moz-transform", "");
+    $("#icone_fa_github").css("-webkit-transform", "");
+  });
+
+  //====================================================================//
